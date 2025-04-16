9f6bfbac4793cabe4a8b72ca15adcd2a6870ba16
Andrés Ramos <andreschennai@gmail.com>
2025-04-16 12:56:41 +0000

    update

--- config/redis.ts
+++ config/redis.ts
@@ -3,23 +3,27 @@ import { InferConnections } from '@adonisjs/redis/types'
 import { URL } from 'url'
 import env from '#start/env'
 
-const redisUrl = new URL(env.get('REDIS_URL'))
-console.log('[REDIS]: ' + redisUrl)
-console.log('[REDIS host]: ' + redisUrl.hostname)
-console.log('[REDIS port]: ' + redisUrl.port)
-console.log('[REDIS password]: ' + redisUrl.password)
+// const redisUrl = new URL(env.get('REDIS_URL'))
+// console.log('[REDIS]: ' + redisUrl)
+// console.log('[REDIS host]: ' + redisUrl.hostname)
+// console.log('[REDIS port]: ' + redisUrl.port)
+// console.log('[REDIS password]: ' + redisUrl.password)
 
 const redisConfig = defineConfig({
   connection: 'main',
 
   connections: {
     main: {
-      host: redisUrl.hostname,
-      port: Number(redisUrl.port),
-      password: redisUrl.password,
-      db: 0,
-      keyPrefix: '',
-      tls: {},
+      // host: redisUrl.hostname,
+      // port: Number(redisUrl.port),
+      // password: redisUrl.password,
+      // db: 0,
+      // keyPrefix: '',
+      // tls: {},
+      // retryStrategy(times) {
+      //   return times > 10 ? null : times * 50
+      // },
+      url: env.get('REDIS_URL'),
       retryStrategy(times) {
         return times > 10 ? null : times * 50
       },
